03c86b21beb9d5e17673835ed29e8ad1a2a54c13
skerrey <sethkerrey@gmail.com>
2023-02-22 17:30:49 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarComponent from "./components/stripe/Navbar";
+import LoginNav from "./components/firebase/LoginNav";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cancel from "./pages/Cancel";
 import Store from "./pages/Store";
 import Success from "./pages/Success";
+import Navbar from "./components/stripe/Navbar";
 import CartProvider from "./contexts/CartContext";
 import AuthProvider from "./contexts/AuthContext";
 import Dashboard from "./components/firebase/Dashboard";
-import Home from "./components/firebase/Home";
 import PrivateRoute from "./components/firebase/PrivateRoute";
 import Signup from "./components/firebase/Signup";
 import Login from "./components/firebase/Login";
@@ -23,13 +23,14 @@ function App() {
       <AuthProvider>
       <CartProvider>
         <Container>
-          <NavbarComponent></NavbarComponent>
+
+          <Navbar />
           <Router>
+          <LoginNav />
             <Routes>
               <Route index element={<Store />} />
               <Route path="success" element={<Success />} />
               <Route path="cancel" element={<Cancel />} />
-              <Route path="/home" element={<Home />} />
               <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
               <Route path="/settings" element={<PrivateRoute><UpdateProfile /></PrivateRoute>}/>
               <Route path="/signup" element={<Signup />} />
