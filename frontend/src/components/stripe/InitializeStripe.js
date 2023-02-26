16aa8d66a24937a88735db6a8deaa8124e792a7d
skerrey <sethkerrey@gmail.com>
2023-02-26 16:35:33 -0600

    Create InitializeStripe.js

---
+++ frontend/src/components/stripe/InitializeStripe.js
@@ -0,0 +1,8 @@
+import {loadStripe} from '@stripe/stripe-js';
+
+const initializeStripe = async () => {
+  const stripe = await loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+  return stripe;
+};
+
+export default initializeStripe;
