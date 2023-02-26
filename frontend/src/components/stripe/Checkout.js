67402904a845a6d7eeae3d8fa3149f3a60ad9912
skerrey <sethkerrey@gmail.com>
2023-02-26 16:35:36 -0600

    Create Checkout.js

---
+++ frontend/src/components/stripe/Checkout.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import db from "../../firebase";
+
+function Checkout() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    db.collection("products")
+      .where("active", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        const products = {};
+        querySnapshot.forEach(async (productDoc) => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection("prices").get();
+          priceSnap.docs.forEach((price) => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data(),
+            };
+          });
+        });
+        setProducts(products);
+      });
+  }, []);
+  
+  console.log(products + "products")
+
+  return <div className="checkout"></div>;
+
+}
+
+export default Checkout;
