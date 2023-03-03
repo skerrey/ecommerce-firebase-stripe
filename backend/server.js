b35de38dbf4f7d338497d47529902a92f7089248
skerrey <sethkerrey@gmail.com>
2023-03-02 19:10:06 -0600

    Add stripe payment element to server

--- backend/server.js
+++ backend/server.js
@@ -38,4 +38,29 @@ app.post("/checkout", async (req, res) => {
 
 });
 
+// -------------------Stripe Element-------------------
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400; 
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
 app.listen(4000, () => console.log("Server started on port 4000"));
