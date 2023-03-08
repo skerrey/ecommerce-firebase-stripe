c02af1467b9fd6b5afbcf59da10ebfebf62298d3
skerrey <sethkerrey@gmail.com>
2023-03-08 16:34:08 -0600

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,6 +4,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+// Version 9
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+
 const firebaseConfig ={
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,14 +19,14 @@ const firebaseConfig ={
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// const app = firebase.initializeApp(firebaseConfig); 
-// export const auth = app.auth();
-// export const db = app.firestore();
-// export default app;
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
 
-  const app = firebase.initializeApp(firebaseConfig);
-  const auth = app.auth();
-  const db = app.firestore();
+// Version 9
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
 
 export { app, auth, db };
 
