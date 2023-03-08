985e6bfb559493b490f28bff80a7fa65b86469f0
skerrey <sethkerrey@gmail.com>
2023-03-08 13:09:23 -0600

    Update ProductCard.js

--- frontend/src/components/stripe/ProductCard.js
+++ frontend/src/components/stripe/ProductCard.js
@@ -3,7 +3,8 @@
 import { Card, Button, Form, Row, Col } from "react-bootstrap";
 import { CartContext } from "../../contexts/CartContext";
 import { useContext, useState } from "react";
-import './ProductCard.css'
+import "./ProductCard.css";
+import { BsFillTrash2Fill, BsFillCartCheckFill } from "react-icons/bs";
 
 function ProductCard(props) { // props.product is the product we are selling
   const product = props.product;
@@ -12,7 +13,7 @@ function ProductCard(props) { // props.product is the product we are selling
   console.log(cart.items);
 
   const [show, setShow] = useState(false); // Handles show upon mouse enter/leave
-  const [clicked, setClicked] = useState(false); // Handles button appearance when clicked
+  const [clicked, setClicked] = useState(false); // Handles button visibility when clicked
 
   function onMouseEnter() { // When the mouse enters the card, show the button "Add to Cart"
     setShow(true);
@@ -25,11 +26,9 @@ function ProductCard(props) { // props.product is the product we are selling
   }
 
   function isClicked() { // When the mouse clicks the "Add to Cart" button, buttons remain visible
-    setClicked(true);
-  }
-
-  function isClickedAgain() { // When the mouse clicks the "Delete" button, buttons disappear
-    setClicked(false);
+    if (clicked === false) {
+      setClicked(true);
+    } else { setClicked(false); }
   }
 
   return (
@@ -49,21 +48,31 @@ function ProductCard(props) { // props.product is the product we are selling
 
             { // If the product is in the cart, display the quantity and buttons to add or remove one from the cart 
             show && (productQuantity > 0 ?
-              <div className="card-up" style={{ animation : isClicked ? "card-up ease-in-out 0.6s"
-              : isClickedAgain(true) ? "card-down ease-in-out 1s"
-              : "none" }}>
-                <p className="pt-2">Add to Cart</p>
+              <div className="card-up" style={{ animation : isClicked ? "card-up ease-in-out 0.6s" : "none" }}>
+                <p className="pt-2">Add To Cart</p>
                 <Form as={Row}>
                   <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
                   <Col sm="6">
-                    <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className="mx-2">+</Button>
-                    <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className="mx-2">-</Button>
+                    <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className="plus-min-del">-</Button>
+                    <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className="plus-min-del">+</Button>
                   </Col>
                 </Form>
-                <Button variant="danger" className="" onClick={(e) => { 
-                  cart.deleteFromCart(product.id); 
-                  isClickedAgain(e);
-                }}>Delete</Button>
+                <Row className="justify-content-around">
+                  <Col sm="6">
+                    <Button className="plus-min-del" variant="danger" onClick={(e) => { 
+                      cart.deleteFromCart(product.id); 
+                      isClicked(e);
+                    }}>
+                      <BsFillTrash2Fill />
+                    </Button>
+                  </Col>
+                  <Col sm="6" className="my-2">
+                    <Button variant="success">
+                      Checkout <BsFillCartCheckFill />
+                    </Button>
+                  </Col>
+                </Row>
+
               </div> 
               :
               <Button className="card-button-add-to-cart" onClick={(e) => {
