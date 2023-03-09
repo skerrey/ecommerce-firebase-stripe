d0f8e945fe081681a828e640007788fc6b9b7b60
skerrey <sethkerrey@gmail.com>
2023-03-09 08:16:09 -0600

    Update CartProduct.js

--- frontend/src/components/stripe/CartProduct.js
+++ frontend/src/components/stripe/CartProduct.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import { CartContext } from "../../contexts/CartContext";
 import { useContext } from "react";
 import { getProductData } from "../../productStore";
+import "./CartProduct.css";
 
 function CartProduct(props) {
   const cart = useContext(CartContext);
@@ -13,11 +14,14 @@ function CartProduct(props) {
 
   return (
     <>
-      <h3>{productData.title}</h3>
-      <p>{quantity} total</p>
-      <p>${ (quantity * productData.price).toFixed(2) }</p>
-      <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
-      <hr />
+      <div className="">
+        <img src={productData.image} alt={productData.imageAlt} className="product-image " />
+        <h3>{productData.title}</h3>
+        <p>{quantity} total</p>
+        <p>${ (quantity * productData.price).toFixed(2) }</p>
+        <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
+        <hr />
+      </div>
     </>
   )
 }
