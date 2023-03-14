0c690a990c0b65e39b50208f3fd49ff4ffdbc87b
skerrey <sethkerrey@gmail.com>
2023-03-14 18:32:33 -0500

    Update CartProduct.js

--- frontend/src/components/stripe/CartProduct.js
+++ frontend/src/components/stripe/CartProduct.js
@@ -1,9 +1,10 @@
 // Description: Show cart products in modal
 
-import Button from "react-bootstrap/Button";
 import { CartContext } from "../../contexts/CartContext";
 import { useContext } from "react";
 import { getProductData } from "../../productStore";
+import { BsFillTrash2Fill } from "react-icons/bs";
+import { BiPlusCircle } from "react-icons/bi";
 import "./CartProduct.css";
 
 function CartProduct(props) {
@@ -14,12 +15,23 @@ function CartProduct(props) {
 
   return (
     <>
-      <div className="">
-        <img src={productData.image} alt={productData.imageAlt} className="product-image " />
-        <h3>{productData.title}</h3>
-        <p>{quantity} total</p>
-        <p>${ (quantity * productData.price).toFixed(2) }</p>
-        <Button size="sm" onClick={() => cart.deleteFromCart(id)}>Remove</Button>
+      <div className="prod-box">
+        <div className="title">
+          {productData.title}
+          <div className="sub-title">${productData.price}</div>
+        </div>
+        <div className="image-box">
+          <img src={productData.image} alt={productData.imageAlt} className="product-image " />
+        </div>
+        <div className="info-box">
+          {quantity === 1 ?
+            <div className="btn btn-custom ps-2" size="sm" onClick={() => cart.deleteFromCart(id)}><BsFillTrash2Fill style={{color: "#aa4a44"}} /></div>
+            :
+            <div className="btn btn-custom" onClick={() => cart.removeOneFromCart(productData.id)}>-</div>
+        } 
+          <div className="quantity">{quantity}</div>
+          <div className="btn btn-custom float-right" onClick={() => cart.addOneToCart(productData.id)}>+</div>
+        </div>
         <hr />
       </div>
     </>
