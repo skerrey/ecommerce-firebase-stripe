cdfa12b814d25cf2f34db5f3e3514e3c6761b207
skerrey <sethkerrey@gmail.com>
2023-03-16 12:33:27 -0500

    Update Checkout.js

--- frontend/src/components/stripe/Checkout.js
+++ frontend/src/components/stripe/Checkout.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CartContext } from "../../contexts/CartContext";
-import CartProduct from './CartProduct';
+import CartProduct from "./CartProduct";
 import CheckoutForm from "./CheckoutForm";
 import { Row, Col } from "react-bootstrap";
+import "./Checkout.css";
+import Responsive from "../../helpers/Responsive";
 
 const stripePromise = loadStripe("pk_test_GKcjZ2vSJh0CsQIHg4FRDXuD00VJwUDHV3");
 
 function Checkout() {
+  const { isMobile } = Responsive();
   const cart = useContext(CartContext);
-
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
@@ -18,14 +20,14 @@ function Checkout() {
     fetch("http://localhost:4000/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: cart.getTotalCost() }),
+      body: JSON.stringify({ items: cart.getTotalCost().toFixed(2) }),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
   }, []);
 
   const appearance = {
-    theme: 'stripe',
+    theme: "stripe",
   };
   const options = {
     clientSecret,
@@ -34,20 +36,46 @@ function Checkout() {
 
   return (
     <>
-      <p>Items in your cart:</p>
-      <Row xs={1} md={3} className="g-0">
-        {cart.items.map((currentProduct, index) => (
-          <CartProduct key={index} id={currentProduct.id} quantity={currentProduct.quantity} />
-        ))}
-        <h2>Total: ${cart.getTotalCost().toFixed(2)}</h2>
-      </Row>
-      <div>
-        {clientSecret && (
-          <Elements options={options} stripe={stripePromise}>
-            <CheckoutForm />
-          </Elements>
-        )}
-      </div>
+      {!isMobile() ? // Check if mobile or desktop
+        <>
+          <h4 className="pt-3">Items in your cart:</h4>
+          <div className="cart-box">
+            <div className="product-box">
+              <div xs={1} className="g-0">
+                {cart.items.map((currentProduct, index) => (
+                  <CartProduct key={index} id={currentProduct.id} quantity={currentProduct.quantity} />
+                ))}
+              </div>
+            </div>
+  
+            <div className="checkout-box">
+              <h2>Total: ${cart.getTotalCost().toFixed(2)}</h2>
+              {clientSecret && (
+                <Elements options={options} stripe={stripePromise}>
+                  <CheckoutForm />
+                </Elements>
+              )}
+            </div>
+          </div>
+        </>
+        :
+        <>
+          <h4 className="pt-3">Items in your cart:</h4>
+          <Row xs={1} md={3} className="g-0">
+            {cart.items.map((currentProduct, index) => (
+              <CartProduct key={index} id={currentProduct.id} quantity={currentProduct.quantity} />
+            ))}
+            <h2>Total: ${cart.getTotalCost().toFixed(2)}</h2>
+          </Row>
+          <div className="pb-5">
+            {clientSecret && (
+              <Elements options={options} stripe={stripePromise}>
+                <CheckoutForm />
+              </Elements>
+            )}
+          </div>
+        </>
+      }
     </>
   );
 }
