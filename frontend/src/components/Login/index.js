542c6fe9455046cf8186b023380b744f9e7890f0
skerrey <sethkerrey@gmail.com>
2023-03-18 18:45:25 -0500

    Add styling

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -4,6 +4,7 @@ import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import "./style.css"
 
 export default function Login() {
   const emailRef = useRef();
@@ -34,7 +35,7 @@ export default function Login() {
       <div className="parent-container">
         <div className="child-container">
 
-          <Card>
+          <Card className="login-card">
             <Card.Body>
               <h2 className="text-center mb-4">Log In</h2>
               {error && <Alert variant="danger">{error}</Alert>}
