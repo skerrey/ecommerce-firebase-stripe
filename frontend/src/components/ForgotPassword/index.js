ab9ef730baa156403d5ee93388674abaf49801a7
skerrey <sethkerrey@gmail.com>
2023-03-18 18:54:49 -0500

    Remove excess divs

--- frontend/src/components/ForgotPassword/index.js
+++ frontend/src/components/ForgotPassword/index.js
@@ -4,6 +4,7 @@ import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../../contexts/AuthContext';
 import { Link } from 'react-router-dom';
+import "./style.css"
 
 export default function ForgotPassword() {
   const emailRef = useRef();
@@ -30,32 +31,28 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <div className="parent-container">
-        <div className="child-container">
-          <Card>
-            <Card.Body>
-              <h2 className="text-center mb-4">Password Reset</h2>
-              {error && <Alert variant="danger">{error}</Alert>}
-              {message && <Alert variant="success">{message}</Alert>}
-              <Form onSubmit={handleSubmit}>
-                <Form.Group id="email">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" ref={emailRef} required />
-                </Form.Group>
-                <Button disabled={loading} className="w-100 mt-3" type="submit">
-                  Rest Password
-                </Button>
-              </Form>
-              <div className="w-100 text-center mt-3">
-                <Link to="/login">Login</Link>
-                <div className='mt-2'>
-                  Need an account? <Link to="/signup">Sign Up</Link>
-                </div>
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
+      <Card className="forgot-password-card">
+        <Card.Body>
+          <h2 className="text-center mb-4">Password Reset</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100 mt-3" type="submit">
+              Rest Password
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/login">Login</Link>
+            <div className='mt-2'>
+              Need an account? <Link to="/signup">Sign Up</Link>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
     </>
   )
 }
