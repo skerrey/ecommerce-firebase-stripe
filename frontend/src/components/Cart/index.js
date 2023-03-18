c8b78d9687cc20226565a3e1b6b54bb8ca51ff31
skerrey <sethkerrey@gmail.com>
2023-03-18 18:05:46 -0500

    Update index.js

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -14,25 +14,23 @@ function Cart(props) {
 
   return (
     <>
-      <div className="prod-box">
-        <div className="title">
-          {productData.title}
-          <div className="sub-title">${productData.price}</div>
-        </div>
-        <div className="image-box">
-          <img src={productData.image} alt={productData.imageAlt} className="product-image " />
-        </div>
-        <div className="info-box">
-          {quantity === 1 ?
-            <div className="btn btn-custom ps-2" size="sm" onClick={() => cart.deleteFromCart(id)}><BsFillTrash2Fill style={{color: "#aa4a44"}} /></div>
-            :
-            <div className="btn btn-custom" onClick={() => cart.removeOneFromCart(productData.id)}>-</div>
-        } 
-          <div className="quantity">{quantity}</div>
-          <div className="btn btn-custom float-right" onClick={() => cart.addOneToCart(productData.id)}>+</div>
-        </div>
-        <hr />
+      <div className="cart-title">
+        {productData.title}
+        <div className="cart-sub-title">${productData.price}</div>
       </div>
+      <div className="cart-image-box">
+        <img src={productData.image} alt={productData.imageAlt} className="cart-product-image " />
+      </div>
+      <div className="cart-info-box">
+        {quantity === 1 ?
+          <div className="btn cart-btn-custom ps-2" size="sm" onClick={() => cart.deleteFromCart(id)}><BsFillTrash2Fill style={{color: "#aa4a44"}} /></div>
+          :
+          <div className="btn cart-btn-custom" onClick={() => cart.removeOneFromCart(productData.id)}>-</div>
+      } 
+        <div className="cart-quantity">{quantity}</div>
+        <div className="btn cart-btn-custom float-right" onClick={() => cart.addOneToCart(productData.id)}>+</div>
+      </div>
+      <hr className="w-75 mx-auto" />
     </>
   )
 }
