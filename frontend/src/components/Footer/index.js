62aa40cbfa4bf68e8b317ef75b7fe74e19683b37
skerrey <sethkerrey@gmail.com>
2023-03-18 18:45:51 -0500

    Update index.js

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,1 +1,39 @@
 // Description: Footer component
+
+import React, { useState } from "react";
+import { useNavigate, Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import { Container } from "react-bootstrap";
+import "./style.css";
+
+export default function Footer() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const [, setError] = useState('');
+
+  async function handleLogout() { 
+    setError('');
+    try {
+      await logout();
+      navigate('/login');
+    } catch {
+      setError('Failed to log out');
+    }
+  }
+
+  return (
+    <> 
+      <div className="fixed-bottom text-center pb-5">
+        <Container>
+          <div>
+            <Link to="/">Home</Link> | <Link to="/dashboard">Dashboard</Link> | <Link to="/settings">Settings</Link> | <Link onClick={handleLogout}>Logout</Link>
+          </div>
+          <div className="fst-italic">
+            Copyright &copy; 2023 by <a href="https://sethkerrey.com/">Seth Kerrey</a>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
