bd7d1025961c11a166d186bf10ed9919e5785834
skerrey <sethkerrey@gmail.com>
2023-03-18 18:46:03 -0500

    Add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import UpdateProfile from "./components/UpdateProfile/index";
 import ForgotPassword from "./components/ForgotPassword/index";
 import Navigation from "./components/Navigation/index";
 import Checkout from "./components/Checkout/index";
+import Footer from "./components/Footer/index";
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
                 <Route path="/forgot-password" element={<ForgotPassword />} />
               </Routes>
             </Container>
+            <Footer />
           </Router>
 
       </CartProvider>
