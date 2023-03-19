1c9f7a28fce6282f375e2d42403e58390f1a29a5
skerrey <sethkerrey@gmail.com>
2023-03-18 19:33:10 -0500

    Update index.js

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
 
 export default function Footer() {
@@ -24,15 +24,17 @@ export default function Footer() {
 
   return (
     <> 
-      <div className="fixed-bottom text-center pb-5">
-        <Container>
-          <div>
+      <div className="footer fixed-bottom">
+        <Row>
+          <Col>
             <Link to="/">Home</Link> | <Link to="/dashboard">Dashboard</Link> | <Link to="/settings">Settings</Link> | {currentUser ? <Link onClick={handleLogout}>Logout</Link> : <Link to="/login">Login</Link>}
-          </div>
-          <div className="fst-italic">
+          </Col>
+        </Row>
+        <Row>
+          <Col className="fst-italic">
             Copyright &copy; 2023 by <a href="https://sethkerrey.com/">Seth Kerrey</a>
-          </div>
-        </Container>
+          </Col>
+        </Row>
       </div>
     </>
   );
