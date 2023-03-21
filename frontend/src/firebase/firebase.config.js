13abc061d06f0d95cf45404b8d0eece43d1c4775
skerrey <sethkerrey@gmail.com>
2023-03-21 16:35:22 -0500

    Rename config

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -14,7 +14,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig ={
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: "auth-production-f8af8",
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
