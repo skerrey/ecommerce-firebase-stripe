db4a94acd234c2dbd845106337caa212e0cd3d25
skerrey <sethkerrey@gmail.com>
2023-03-23 17:37:07 -0500

    Update index.js

--- frontend/src/components/CheckoutForm/index.js
+++ frontend/src/components/CheckoutForm/index.js
@@ -66,6 +66,7 @@ export default function CheckoutForm() {
       },
     });
 
+
     // This point will only be reached if there is an immediate error when
     // confirming the payment. Otherwise, your customer will be redirected to
     // your `return_url`. For some payment methods like iDEAL, your customer will
