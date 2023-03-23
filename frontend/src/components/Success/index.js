4360d64afb4ad65ca43efa4164ff79cd6d0cbce3
skerrey <sethkerrey@gmail.com>
2023-03-23 11:55:43 -0500

    Create index.js

---
+++ frontend/src/components/Success/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Success() {
+  return (
+    <>
+      <div align="center" className="p-3">
+        <h1>Payment Successful!</h1>
+        <p>Your payment of XXX was processed successfully you may not return to the store.</p>
+        <Link to="/">Return to Store</Link>
+      </div>
+    </>
+  )
+}
