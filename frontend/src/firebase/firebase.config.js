d0f45b3c5e38955f5198748e725d300865f4f11a
skerrey <sethkerrey@gmail.com>
2023-03-23 11:55:54 -0500

    Update firebase.config.js

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -1,15 +1,8 @@
 // Description: Firebase config
 
-// Version 8 compat
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
-// import 'firebase/compat/firestore';
-
-// Version 9
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 
 const firebaseConfig ={
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -20,16 +13,8 @@ const firebaseConfig ={
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// Version 8 compat
-// const app = firebase.initializeApp(firebaseConfig);
-// const auth = app.auth();
-// const db = app.firestore();
-
-// Version 9
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
-
-
