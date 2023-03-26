e349dc064bbb0c5aa90dc36263a5ba5f0266eb3d
skerrey <sethkerrey@gmail.com>
2023-03-25 22:58:00 -0500

    Update index.js

--- frontend/src/components/CheckoutForm/index.js
+++ frontend/src/components/CheckoutForm/index.js
@@ -94,7 +94,7 @@ export default function CheckoutForm() {
       <PaymentElement id="payment-element" options={paymentElementOptions} />
       <button disabled={isLoading || !stripe || !elements} id="submit">
         <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : <div className="btn btn-primary">Pay now</div>}
+          {isLoading ? <div className="spinner" id="spinner"></div> : <div className="btn-custom">Pay now</div>}
         </span>
       </button>
       {/* Show any error or success messages */}
