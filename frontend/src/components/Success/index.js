ec489ecadec66b0e243897ffb9df1fd026654c26
skerrey <sethkerrey@gmail.com>
2023-03-26 19:45:00 -0500

    Clear cart on success page

--- frontend/src/components/Success/index.js
+++ frontend/src/components/Success/index.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { CartContext } from '../../contexts/CartContext';
 
 export default function Success() {
+  const { clearCart } = useContext(CartContext);
 
-  localStorage.removeItem("cartProducts"); // removes cartProducts from local storage
+  useEffect(() => {
+    clearCart();
+  }, []);
+  
 
   return (
     <>
       <div align="center" className="p-3">
         <h1>Payment Successful!</h1>
-        <p>Your payment of was processed successfully you may not return to the store.</p>
+        <p>Your payment was processed successfully you may not return to the store.</p>
         <Link to="/">Return to Store</Link>
       </div>
     </>
