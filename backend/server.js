081277d9ea6a720066ac126198d78058033e1104
skerrey <sethkerrey@gmail.com>
2023-03-26 21:04:02 -0500

    Remove Stripe checkout

--- backend/server.js
+++ backend/server.js
@@ -11,40 +11,16 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
-app.post("/checkout", async (req, res) => {
 
-  console.log(req.body);
-  const items = req.body.items;
-  let lineItems = [];
-  items.forEach((item) => {
-    lineItems.push(
-      {
-        price: item.id,
-        quantity: item.quantity
-      }
-    )
-  });
-
-  const session = await stripe.checkout.sessions.create({
-    line_items: lineItems,
-    mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel"
-  });
-
-  res.send(JSON.stringify({
-    url: session.url
-  }));
-
-});
-
-// -------------------Stripe Element-------------------
 const calculateOrderAmount = (items) => {
   console.log(`Item total: ${items}`);
   totalCost = items * 100;
   return totalCost;
 };
 
+/**
+ * Payment intent Stripe
+ */
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
 
