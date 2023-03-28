32feb17144917be277725ffb1c27a18f0cc82dcf
skerrey <sethkerrey@gmail.com>
2023-03-27 23:24:11 -0500

    Update index.js

--- frontend/src/components/Checkout/index.js
+++ frontend/src/components/Checkout/index.js
@@ -1,21 +1,24 @@
 // Description: Checkout component that renders the checkout form and cart
 
 import React, { useState, useEffect, useContext } from "react";
+import { Link } from 'react-router-dom';
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CartContext } from "../../contexts/CartContext";
+import { useAuth } from "../../contexts/AuthContext";
 import Cart from "../Cart/index";
 import CheckoutForm from "../CheckoutForm/index";
-import { Row } from "react-bootstrap";
-import "./style.css";
 import Responsive from "../../helpers/Responsive";
 import { AiOutlineDownCircle, AiOutlineUpCircle } from 'react-icons/ai';
+import { Row } from "react-bootstrap";
+import "./style.css";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 function Checkout() {
   const { isMobile } = Responsive();
   const cart = useContext(CartContext);
+  const { currentUser } = useAuth();
   const [clientSecret, setClientSecret] = useState("");
   const [arrowDirection, setArrowDirection] = useState("down");
 
@@ -62,11 +65,10 @@ function Checkout() {
           <div className="checkout-cart-box">
             <div className="checkout-product-box">
               <div className="checkout-scroll-arrow">
-                {arrowDirection === 'down' ? (
+                {arrowDirection === 'down' ? ( // Check if arrow should be up or down
                     <>
                       <AiOutlineDownCircle size={32} />
                     </>
-
                 ) : (
                   <AiOutlineUpCircle size={32} />
                 )}
@@ -80,6 +82,7 @@ function Checkout() {
   
             <div className="checkout-box">
               <h2>Total: ${cart.getTotalCost().toFixed(2)}</h2>
+              {!currentUser ? <div className="pb-3 ps-2 fst-italic">Checkout out guest? <Link to="/login" className="fst-normal">Login</Link></div> : null}
               {clientSecret && (
                 <Elements options={options} stripe={stripePromise}>
                   <CheckoutForm />
