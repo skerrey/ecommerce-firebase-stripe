0e2740846d15886fc8775011389dd411cb36aaca
skerrey <sethkerrey@gmail.com>
2023-03-29 18:28:56 -0500

    Add autoComplete to form

--- frontend/src/components/Signup/index.js
+++ frontend/src/components/Signup/index.js
@@ -60,23 +60,23 @@ export default function Signup() {
           <Form onSubmit={handleSubmit}>
             <Form.Group id="first-name">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" ref={firstNameRef} required />
+              <Form.Control type="text" autoComplete="first-name" ref={firstNameRef} required />
             </Form.Group>
             <Form.Group id="last-name">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" ref={lastNameRef} required />
+              <Form.Control type="text" autoComplete="family-name" ref={lastNameRef} required />
             </Form.Group>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control type="email" autoComplete="email" ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password" className="my-2">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+              <Form.Control type="password" autoComplete="new-password" ref={passwordRef} required />
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
+              <Form.Control type="password" autoComplete="new-password" ref={passwordConfirmRef} required />
             </Form.Group>
             <Button disabled={loading} className="w-100 mt-3" type="submit">
               Sign Up
