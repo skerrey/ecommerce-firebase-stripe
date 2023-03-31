62f424165053dfd42dc6bfd2cf9481cd781c7140
skerrey <sethkerrey@gmail.com>
2023-03-31 16:34:02 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
       <AuthProvider>
       <CartProvider>
 
-          <Router>
+          <Router basename="/">
             <Navigation />
             <Container>
               <Routes>
