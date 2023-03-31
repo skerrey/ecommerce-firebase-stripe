d733182b0856628719a2e43701c5e0ddaab74729
skerrey <sethkerrey@gmail.com>
2023-03-31 17:41:12 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { Hashrouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { createHashHistory } from 'history';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const history = createHashHistory();
+
 root.render(
-  <Router>
+  <Router history={history}>
     <App />
   </Router>
 );
