9c6db03dd43d856f2f574e1f483942769a034604
skerrey <sethkerrey@gmail.com>
2023-03-30 19:09:54 -0500

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Container } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Store from "./components/Store/index";
 import CartProvider from "./contexts/CartContext";
 import AuthProvider from "./contexts/AuthContext";
@@ -22,7 +22,7 @@ function App() {
       <AuthProvider>
       <CartProvider>
 
-
+          <Router>
             <Navigation />
             <Container>
               <Routes>
@@ -37,7 +37,7 @@ function App() {
               </Routes>
             </Container>
             <Footer />
-
+          </Router>
 
       </CartProvider>
       </AuthProvider>
